07e0aff8c94ba72383f22959cc08047065487bae
Martina Mucci <martina.m.mucci@pwc.com>
2020-12-07 21:14:42 -0300

    Add project files.

---
+++ ACM.BL/Customer.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ACM.BL
+{
+    public class Customer
+    {
+        public int CustomerId { get; private set; } // propg snippet
+        public string EmailAddress { get; set; } // prop snippet
+        public string FullName
+        {
+            get
+            {
+                return LastName + " " + FirstName;
+            }
+        }
+        public string FirstName { get; set; } // Auto-Implemented Property - Behind the scenes the code is still creating a backing field
+                                              // This is used when there is no need for extra code in the getter or setter of the property
+        private string _LastName; // Private Backing Field - it holds the data and no code outside of this class can access it
+
+        public string LastName // this C# property makes the data accessible to other parts of the application
+        {
+            get
+            {
+                // ...
+                return _LastName;
+            }
+            set
+            {
+                // ...
+                _LastName = value;
+            }
+        }
+    }
+}
